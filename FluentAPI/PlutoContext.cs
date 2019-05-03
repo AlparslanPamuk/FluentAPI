59735ec911390b64dad62993a166c10909be0452
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-05-03 17:10:48 +0300

    Second Commit

--- FluentAPI/PlutoContext.cs
+++ FluentAPI/PlutoContext.cs
@@ -12,5 +12,34 @@ namespace DataAnnotations
         public virtual DbSet<Author> Authors { get; set; }
         public virtual DbSet<Course> Courses { get; set; }
         public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(2000);
+
+            modelBuilder.Entity<Course>()
+                .HasRequired(c => c.Author)
+                .WithMany(a => a.Courses)
+                .HasForeignKey(c => c.AuthorID)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Tags)
+                .WithMany(t => t.Courses)
+                .Map(m => m.ToTable("CourseTags"));
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
